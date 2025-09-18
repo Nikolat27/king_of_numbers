96e7f5a473b93d78f1af6d2aac9b6c243ce9e0ef
Sam <samalizadeh899@gmail.com>
2025-09-18 16:39:48 +0330

    Modified the user schema and Added get func

--- backend/database/models/users.go
+++ backend/database/models/users.go
@@ -2,9 +2,13 @@ package models
 
 import (
 	"context"
+	"time"
+
+	"github.com/Nikolat27/king_of_numbers/backend/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"time"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type UserModel struct {
@@ -12,10 +16,10 @@ type UserModel struct {
 }
 
 type User struct {
-	Id        primitive.ObjectID
-	Username  string
-	Password  string
-	CreatedAt time.Time
+	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username  string             `bson:"username" json:"username"`
+	Password  string             `bson:"password" json:"password"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 }
 
 func NewUserModel(db *mongo.Database) *UserModel {
@@ -24,7 +28,7 @@ func NewUserModel(db *mongo.Database) *UserModel {
 	}
 }
 
-func (user *UserModel) Create(username, password string) (primitive.ObjectID, error) {
+func (user *UserModel) Create(username, password string) (primitive.ObjectID, *types.ErrorResponse) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -36,8 +40,26 @@ func (user *UserModel) Create(username, password string) (primitive.ObjectID, er
 
 	result, err := user.collection.InsertOne(ctx, newUser)
 	if err != nil {
-		return primitive.NilObjectID, err
+		return primitive.NilObjectID, types.NewErrorResponse("create user", err)
 	}
 
 	return result.InsertedID.(primitive.ObjectID), nil
+}
+
+func (user *UserModel) Get(filter, projection bson.M) (*User, *types.ErrorResponse) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var result User
+
+	findOptions := options.FindOne()
+	if projection != nil {
+		findOptions.SetProjection(projection)
+	}
+
+	if err := user.collection.FindOne(ctx, filter, findOptions).Decode(&result); err != nil {
+		return nil, types.NewErrorResponse("getUser", err)
+	}
+
+	return &result, nil
 }
