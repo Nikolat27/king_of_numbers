050998638c27429ce4c028afdfa8fdc9d38519b3
Sam <samalizadeh899@gmail.com>
2025-09-18 15:44:51 +0330

    Modified the error response

--- backend/types/types.go
+++ backend/types/types.go
@@ -4,3 +4,33 @@ type ErrorResponse struct {
 	Type   string
 	Detail string
 }
+
+func NewErrorResponse(typ, detail any) *ErrorResponse {
+	var (
+		typString    string
+		detailString string
+	)
+
+	switch v := typ.(type) {
+	case string:
+		typString = v
+	case error:
+		typString = v.Error()
+	default:
+		typString = "unknown 'typ' data type"
+	}
+
+	switch v := detail.(type) {
+	case string:
+		detailString = v
+	case error:
+		detailString = v.Error()
+	default:
+		detailString = "unknown 'detail' data type"
+	}
+
+	return &ErrorResponse{
+		Type:   typString,
+		Detail: detailString,
+	}
+}
